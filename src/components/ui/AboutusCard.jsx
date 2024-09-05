74f35b10d4196d3f7067f10081c32a4898df5cee
Chandan kohar <165137795+Chandankohar@users.noreply.github.com>
2024-09-05 22:33:09 +0530

    Update AboutusCard.jsx

--- src/components/ui/AboutusCard.jsx
+++ src/components/ui/AboutusCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutusCard = ({ image, name, address, contact,work,role }) => {
   return (
-    <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-4 shadow-lg flex flex-col  text-center  sm:w-1/3 m-4">
+    <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-4 shadow-lg m-4  flex border flex-col  md:m-2 xl:m-0 shadow  rounded-2xl">
     <img src={image} alt={name} className="h-2/3 w-full rounded-xl object-cover" />
     <h2 className="text-xl font-semibold text-white">{name}</h2>
     <p className="text-gray-300">{address}</p>
